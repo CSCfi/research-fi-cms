fc3f377c2a79dc1b04188d3bb9ba7ec9942cdd95
Timo Särkikoski <sarkikos@gmail.com>
2020-08-24 15:16:11 +0300

    Set Django debug mode from env variable

--- django/cms/settings.py
+++ django/cms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DJANGO_ENV_SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_ENV_DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '.rahtiapp.fi', '.csc.fi']
 
