ca2626c141313a9982d610d34179270a194b539f
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-30 12:03:27 +0200

    Register figures

--- django/contentapp/admin.py
+++ django/contentapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Page, Shortcut
+from .models import Page, Shortcut, Figure, SingleFigure
 
 # Register your models here.
 admin.site.register(Page)
 admin.site.register(Shortcut)
+admin.site.register(Figure)
+admin.site.register(SingleFigure)
