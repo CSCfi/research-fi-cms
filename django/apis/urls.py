7ae7aa195585e26cb71a00050d277ccd6d4a31cb
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-30 12:04:12 +0200

    Figure url

--- django/apis/urls.py
+++ django/apis/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import PostViewSet, ShortcutViewSet
+from .views import PostViewSet, ShortcutViewSet, FigureViewSet
 from rest_framework.routers import DefaultRouter
 
 # Default router lists views
 router = DefaultRouter()
 router.register(r'pages', PostViewSet)
 router.register(r'shortcuts', ShortcutViewSet)
+router.register(r'figure', FigureViewSet)
 urlpatterns = router.urls
