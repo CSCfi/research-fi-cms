1ab3a342607d4b3b4037519914d82677e7670b7f
saulipurhonen <sauli.purhonen@gmail.com>
2020-11-26 11:19:08 +0200

    Removed placement id from figure forms

--- django/contentapp/forms.py
+++ django/contentapp/forms.py
@@ -5,7 +5,7 @@ from ckeditor.widgets import CKEditorWidget
 class PageForm(forms.ModelForm):
     class Meta:
         model = Page
-        fields = ['title_fi', 'title_sv', 'title_en', 'content_fi', 'content_sv', 'content_en']
+        fields = ['page_id', 'title_fi', 'title_sv', 'title_en', 'content_fi', 'content_sv', 'content_en']
     def __init__(self, *args, **kwargs):
         super(PageForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
@@ -14,7 +14,7 @@ class PageForm(forms.ModelForm):
 class ShortcutForm(forms.ModelForm):
     class Meta:
         model = Shortcut
-        fields = ['placement_id', 'title_fi', 'title_sv', 'title_en', 'content_fi', 'content_sv', 'content_en', 'image', 'link']
+        fields = ['placement_id', 'title_fi', 'title_sv', 'title_en', 'content_fi', 'content_sv', 'content_en', 'image', 'image_alt_fi', 'image_alt_sv', 'image_alt_en', 'link']
     def __init__(self, *args, **kwargs):
         super(ShortcutForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
@@ -24,7 +24,7 @@ class SingleFigureForm(forms.ModelForm):
     class Meta:
         model = SingleFigure
         fields = ['figure', 'title_fi', 'title_sv', 'title_en', 'description_fi', 'description_sv', 'description_en', 'thumbnail', 'iframe_fi', 'iframe_sv', 'iframe_en',
-        'link_id', 'source_fi', 'source_sv', 'source_en', 'info_fi', 'info_sv', 'info_en', 'roadmap']
+        'source_fi', 'source_sv', 'source_en', 'info_fi', 'info_sv', 'info_en', 'roadmap']
     def __init__(self, *args, **kwargs):
         super(SingleFigureForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
