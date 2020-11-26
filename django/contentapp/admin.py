bb941bfb7fdbc0afa8108b05d8fdab82a24fff13
saulipurhonen <sauli.purhonen@gmail.com>
2020-11-26 11:19:37 +0200

    Register sector and organization models

--- django/contentapp/admin.py
+++ django/contentapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Page, Shortcut, Figure, SingleFigure
+from .models import Page, Shortcut, Figure, SingleFigure, Sector, Organization
 
 # Register your models here.
 admin.site.register(Page)
 admin.site.register(Shortcut)
 admin.site.register(Figure)
 admin.site.register(SingleFigure)
+admin.site.register(Sector)
+admin.site.register(Organization)
