5f8ade4150e7b1e43f44f30f4995212a011a4c25
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-15 08:46:52 +0200

    Changed ordering of fields in multiple models

--- django/contentapp/models.py
+++ django/contentapp/models.py
@@ -45,6 +45,7 @@ class Page(models.Model):
         
 
 class Shortcut(models.Model):
+    placement_id = models.IntegerField()
     lang = models.CharField(
         max_length=2,
         choices=Lang.choices,
@@ -55,7 +56,6 @@ class Shortcut(models.Model):
         choices=Locations.choices,
         default=Locations.HOME
     )
-    placement_id = models.IntegerField()
     title_fi = models.CharField(max_length=500)
     title_sv = models.CharField(max_length=500, default='Enter title')
     title_en = models.CharField(max_length=500, default='Enter title')
@@ -77,12 +77,12 @@ class Shortcut(models.Model):
         return reverse('shortcut_edit', kwargs={'pk': self.pk})
 
 class Figure(models.Model):
+    placement_id = models.IntegerField()
     lang = models.CharField(
         max_length=2,
         choices=Lang.choices,
         default=Lang.FI
     )
-    placement_id = models.IntegerField()
     title_fi = models.CharField(max_length=500, default='Enter title')
     title_sv = models.CharField(max_length=500, default='Enter title')
     title_en = models.CharField(max_length=500, default='Enter title')
@@ -93,6 +93,8 @@ class Figure(models.Model):
 class SingleFigure(OrderedModelBase):
     figure = models.ForeignKey(Figure, related_name='items', on_delete=models.CASCADE)
     placement_id = models.PositiveIntegerField(editable=True, db_index=True)
+    order_field_name = "placement_id"
+    order_with_respect_to = 'figure'
     lang = models.CharField(
         max_length=2,
         choices=Lang.choices,
@@ -102,20 +104,18 @@ class SingleFigure(OrderedModelBase):
     title_sv = models.CharField(max_length=500, default='Enter title')
     title_en = models.CharField(max_length=500, default='Enter title')
     description_fi = RichTextUploadingField(blank=True)
-    description_en = RichTextUploadingField(blank=True)
     description_sv = RichTextUploadingField(blank=True)
+    description_en = RichTextUploadingField(blank=True)
     thumbnail = models.ImageField(upload_to='images/figures/', null=True, blank=True)
     iframe_fi = models.CharField(max_length=500, blank=True)
-    iframe_en = models.CharField(max_length=500, blank=True)
     iframe_sv = models.CharField(max_length=500, blank=True)
-    order_field_name = "placement_id"
-    order_with_respect_to = 'figure'
+    iframe_en = models.CharField(max_length=500, blank=True)
     source_fi = models.CharField(max_length=500, blank=True)
-    source_en = models.CharField(max_length=500, blank=True)
     source_sv = models.CharField(max_length=500, blank=True)
+    source_en = models.CharField(max_length=500, blank=True)
     info_fi = RichTextUploadingField(blank=True)
-    info_en = RichTextUploadingField(blank=True)
     info_sv = RichTextUploadingField(blank=True)
+    info_en = RichTextUploadingField(blank=True)
     roadmap = models.BooleanField(default=False)
 
     class Meta:
@@ -125,6 +125,7 @@ class SingleFigure(OrderedModelBase):
         return '%d: %s' % (self.placement_id, self.title_fi)
 
 class Sector(models.Model):
+    placement_id = models.PositiveIntegerField(editable=True, db_index=True)
     lang = models.CharField(
         max_length=2,
         choices=Lang.choices,
@@ -136,13 +137,12 @@ class Sector(models.Model):
     subtitle_fi = models.CharField(max_length=500, blank=True)
     subtitle_sv = models.CharField(max_length=500, blank=True)
     subtitle_en = models.CharField(max_length=500, blank=True)
-    placement_id = models.PositiveIntegerField(editable=True, db_index=True)
     iframe_fi = models.CharField(max_length=500, blank=True)
-    iframe_en = models.CharField(max_length=500, blank=True)
     iframe_sv = models.CharField(max_length=500, blank=True)
+    iframe_en = models.CharField(max_length=500, blank=True)
     description_fi = RichTextUploadingField(blank=True)
-    description_en = RichTextUploadingField(blank=True)
     description_sv = RichTextUploadingField(blank=True)
+    description_en = RichTextUploadingField(blank=True)
     icon = models.CharField(max_length=500, default='Add icon code')
 
     class Meta:
@@ -153,6 +153,9 @@ class Sector(models.Model):
 
 class Organization(OrderedModelBase):
     sector = models.ForeignKey(Sector, related_name='organizations', on_delete=models.CASCADE)
+    placement_id = models.PositiveIntegerField(editable=True, db_index=True)
+    order_field_name = "placement_id"
+    order_with_respect_to = 'sector'
     lang = models.CharField(
         max_length=2,
         choices=Lang.choices,
@@ -161,9 +164,6 @@ class Organization(OrderedModelBase):
     name_fi = models.CharField(max_length=500, default='Enter organization name')
     name_sv = models.CharField(max_length=500, default='Enter organization name')
     name_en = models.CharField(max_length=500, default='Enter organization name')
-    placement_id = models.PositiveIntegerField(editable=True, db_index=True)
-    order_field_name = "placement_id"
-    order_with_respect_to = 'sector'
     link = models.CharField(max_length=500, default='Enter link target')
 
     class Meta:
