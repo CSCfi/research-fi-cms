84b3adad0bc08e7c78ab922de5f89b0b5cd7113f
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-18 10:50:28 +0200

    Regex for rahtiapp urls

--- django/cms/settings.py
+++ django/cms/settings.py
@@ -89,6 +89,10 @@ CORS_ALLOWED_ORIGINS = [
     "https://www.research.fi",
 ]
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://\w+\.rahtiapp\.fi$",
+]
+
 ROOT_URLCONF = 'cms.urls'
 
 TEMPLATES = [
