54bdb99ea40fd546ade2525d372a051105a68b5f
Timo Särkikoski <sarkikos@gmail.com>
2024-05-13 16:18:28 +0300

    Add missing commas in settings.py

--- django/cms/settings.py
+++ django/cms/settings.py
@@ -87,8 +87,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://researchfi-qa.2.rahtiapp.fi",
     "https://researchfi-qa-en.2.rahtiapp.fi",
     "https://researchfi-qa-sv.2.rahtiapp.fi",
-    "https://researchfi-production.2.rahtiapp.fi"
-    "https://researchfi-production-en.2.rahtiapp.fi"
+    "https://researchfi-production.2.rahtiapp.fi",
+    "https://researchfi-production-en.2.rahtiapp.fi",
     "https://researchfi-production-sv.2.rahtiapp.fi",
     "https://researchfi-production-staging.rahtiapp.fi",
     "https://researchfi-production-staging.2.rahtiapp.fi",
